8295f70024b0d9052979399ac2aa5fc1b14d38b6
henryfletch <henry@villocq.com>
2014-11-04 17:37:23 +0000

    "pp"

--- BP/errorBars/addErrorBars.m
+++ BP/errorBars/addErrorBars.m
@@ -7,7 +7,7 @@ r = I(:,4); %Block errors per simulation
 n = I(:,2); %Total blocks per simulation
 I(:,6) = calcErrorBars('pos',r,n); %p+ value
 I(:,7) = calcErrorBars('neg',r,n); %p- value
-I(:,6) = abs(I(:,6) - I(:,5)); %P+ length is p+ - y
-I(:,7) = abs(I(:,5) - I(:,7)); %P- length is y - p-
+I(:,6) = I(:,6) - I(:,5); %P+ length is p+ - y
+I(:,7) = I(:,5) - I(:,7); %P- length is y - p-
 
 end
