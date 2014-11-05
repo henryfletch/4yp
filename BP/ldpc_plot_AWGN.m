79114cc1a50627fdc4492450d3e1b545c2b706e0
henryfletch <henry@villocq.com>
2014-11-05 16:55:50 +0000

    uopdate

--- BP/ldpc_plot_AWGN.m
+++ BP/ldpc_plot_AWGN.m
@@ -1,11 +1,11 @@
-%clear
+clear
 
 %Input Data, Z
 U = load('./SavedGraphData/AWGN-uncoded-0to6dB.mat'); %Uncoded
 Z = load('./SavedGraphData/0.5-1008-504-AWGN-3.mat'); %1008x504 LDPC
 Z2 = load('./SavedGraphData/0.5-96-48-AWGN-upto5.43.mat'); %96x48 LDPC
 Z3 = load('./SavedGraphData/0.5-9972-4986-AWGN.mat'); %Irregular 9972x4986
-
+Z4 = load('./SavedGraphData/0.5-1008-504-AWGN-4-xy.mat'); %1008x504 LDPC (CORRECTED!)
 
 %Extract x and y
 x = Z.Z(:,1) - 3;
@@ -14,6 +14,8 @@ x_2 = Z2.Z(:,1) - 3;
 y_2 = smooth(Z2.Z(:,2));
 x_3 = Z3.Z(:,1) - 3;
 y_3 = Z3.Z(:,2);
+x_4 = Z4.x_4;
+y_4 = Z4.y_4;
 x_unenc = U.U(:,1) - 3;
 y_unenc = U.U(:,2);
 
@@ -24,8 +26,9 @@ grid on
 coded1 = semilogy(x,y,'DisplayName','n=1008 r=0.5 LDPC','LineWidth',1.5);
 coded2 = semilogy(x_2,y_2,'DisplayName','n=96 r=0.5 LDPC','LineWidth',1.5);
 coded3 = semilogy(x_3,y_3,'DisplayName','n=9972 r=0.5 irregular LDPC','LineWidth',1.5);
+coded4 = semilogy(x_4,y_4,'DisplayName','n=1008 r=0.5 LDPC (Corrected)','LineWidth',1.5);
 xlabel('Eb/No (dB)');
 ylabel('Bit Error Probability');
 title('AWGN Channel - Performance Curves');
-lgnd = legend([uncoded coded2 coded1 coded3]);
+lgnd = legend([uncoded coded2 coded1 coded4 coded3]);
 set(lgnd,'FontSize',12,'Location','SouthWest');
--- BP/uncoded_graph_test.m
+++ BP/uncoded_graph_test.m
@@ -1,7 +1,7 @@
-x = -3:6;
+x = -3:4;
 x = 10.^(x./10);
 y = 0.5*erfc(sqrt(x));
 
 x = 10*log10(x); 
 
-semilogy(x,y)
+semilogy(x,y,'square')
