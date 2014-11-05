e675825daee2f49e8fbfa6730232a9e9082b1f22
henryfletch <henry@villocq.com>
2014-11-05 17:20:30 +0000

    upate

---
+++ BP/errorBars/errorbars_plot.m
@@ -0,0 +1,20 @@
+clear all
+close all
+
+errorData = load('errorbars-data2.mat');
+I = errorData.I;
+
+x = I(:,1) - 3;
+y = I(:,8); % BLOCK ERROR PROBABILITY NOT BER!
+u = I(:,6);
+l = I(:,7);
+
+graph = semilogy(x,y,'DisplayName','n=96 r=0.5 LDPC','LineWidth',1.5);
+hold on
+grid on
+errorbar(x,y,l,u);
+xlabel('Eb/No (dB)');
+ylabel('Block Error Probability');
+title('AWGN Channel - Block Error Probabilty w/Error Bars ');
+lgnd = legend([graph]);
+set(lgnd,'FontSize',12,'Location','SouthWest');
