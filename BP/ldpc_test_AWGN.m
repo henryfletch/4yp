bbbe7247f311e91cf6f1b6fd008a19b79272c5ea
henryfletch <henry@villocq.com>
2014-11-07 00:24:42 +0000

    b

--- BP/ldpc_test_AWGN.m
+++ BP/ldpc_test_AWGN.m
@@ -12,18 +12,18 @@ G = load('../LDPC data/Rate0.876/G-3584-3140.mat');
 G = G.G; %Dodgy work-around!
 
 % Belief Propogation Iterations
-l = 100;
+l = 200;
 
 % Loop to go over all values of SNR, as well as perform multiple iterations
 I = [];
-for SNR = 8:0.5:10
+for SNR = 0:1:4
     fprintf('SNR =%6.2f',SNR);
     fprintf('\n');
-    tic
-    parfor i = 1:35
-        [~,errRatio(i),iterations(i)] = ldpc_BER_AWGN(G,H,l,SNR);
-    end
-    toc
+    %tic
+    %parfor i = 1:35
+        [~,errRatio(1),iterations(1)] = ldpc_BER_AWGN(G,H,l,SNR);
+    %end
+    %toc
     I = [I;SNR,mean(iterations),mean(errRatio)];
 end
 
