e1327432e0d9cc7df17bf736c78319b1a77d1597
henryfletch <henry@villocq.com>
2014-11-10 22:27:14 +0000

    b

--- BP/ldpc_test_AWGN.m
+++ BP/ldpc_test_AWGN.m
@@ -5,10 +5,12 @@ close
 
 % "Parity Check Matrix" or graph connections
 H = load('../LDPC data/Rate0.5/H-96-48.mat');
+%H = load('../Polar data/H-128-64-polar.mat');
 H = H.H; %Dodgy work-around!
 
 % "Generator Matrix"
 G = load('../LDPC data/Rate0.5/G-96-48.mat');
+%G = load('../Polar data/G-128-64-polar.mat');
 G = G.G; %Dodgy work-around!
 
 % Belief Propogation Iterations
@@ -16,11 +18,11 @@ l = 100;
 
 % Loop to go over all values of SNR, as well as perform multiple iterations
 I = [];
-for SNR = 0:0.5:7
+for SNR = 0:1:7
     fprintf('SNR =%6.2f',SNR);
     fprintf('\n');
     tic;
-    parfor i = 1:10000
+    parfor i = 1:100
         [~,errRatio(i),iterations(i)] = ldpc_BER_AWGN(G,H,l,SNR);
     end
     toc
