cef160f14e861906b736ac1094705f410d14d19b
henryfletch <henry@villocq.com>
2014-11-29 17:53:37 +0000

    test

--- BP/minSum/ldpc_test_minsum_AWGN.m
+++ BP/minSum/ldpc_test_minsum_AWGN.m
@@ -4,24 +4,24 @@ clear
 close
 
 % "Parity Check Matrix" or graph connections
-H = load('../../LDPC data/Rate0.5/H-96-48-v2.mat');
+H = load('../../LDPC data/Rate0.876/H-3584-3140.mat');
 %H = load('../Polar data/H-128-64-polar.mat');
 H = sparse(H.H); %Dodgy work-around!
 
 % "Generator Matrix"
-G = load('../../LDPC data/Rate0.5/G-96-48-v2.mat');
+G = load('../../LDPC data/Rate0.876/G-3584-3141-v2.mat');
 %G = load('../Polar data/G-128-64-polar.mat');
 G = sparse(G.G); %Dodgy work-around!
 
 % Belief Propogation Iterations
-l = 100;
+l = 50;
 
 % Loop to go over all values of SNR, as well as perform multiple iterations
 I = [];
 for SNR = 3:0.5:6
     fprintf('SNR =%6.2f',SNR);
     fprintf('\n');
-    N = floor(1000*SNR^(3));
+    N = floor(100*SNR^(3));
     tic;
     parfor_progress(N);
     parfor i = 1:N
@@ -30,6 +30,6 @@ for SNR = 3:0.5:6
     end
     parfor_progress(0);
     toc
-    I = [I;SNR,mean(iterations),mean(errRatio)];
+    I = [I;SNR,mean(iterations),mean(errRatio),N];
 end
 
