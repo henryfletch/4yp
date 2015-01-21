171bae1912f259a8d80237af6fffc491464b3e67
henryfletch <henry@villocq.com>
2015-01-21 13:20:34 +0000

    update

--- BP/matlab built-in funcs/matlabDecoder.m
+++ BP/matlab built-in funcs/matlabDecoder.m
@@ -1,30 +1,35 @@
 clear
 close
 
+H = dvbs2ldpc(0.9);
+N = 40000;
 I = [];
-for EbNo = -4:0.5:3
-    parfor i = 1:
-        
-    hEnc = comm.LDPCEncoder;
+
+for EbNo = 3.5:0.5:5
+    
+    hEnc = comm.LDPCEncoder(H);
     hMod = comm.PSKModulator(2, 'BitInput',true);
     hChan = comm.AWGNChannel(...
         'NoiseMethod','Signal to noise ratio (Eb/No)','EbNo',EbNo);
     hDemod = comm.PSKDemodulator(2, 'BitOutput',true,...
         'DecisionMethod','Approximate log-likelihood ratio', ...
         'Variance', 1/10^(hChan.SNR/10));
-    hDec = comm.LDPCDecoder;
+    hDec = comm.LDPCDecoder(H);
     hError = comm.ErrorRate;
     
-    data           = logical(randi([0 1], 32400, 1));
-    encodedData    = step(hEnc, data);
-    modSignal      = step(hMod, encodedData);
-    receivedSignal = step(hChan, modSignal);
-    demodSignal    = step(hDemod, receivedSignal);
-    receivedBits   = step(hDec, demodSignal);
-    errorStats     = step(hError, data, receivedBits);
-    bitErr(i) = errorStats(1);
+    parfor_progress(N);
+    parfor i = 1:N
+        data           = logical(randi([0 1], 64800-6480, 1));
+        encodedData    = step(hEnc, data);
+        modSignal      = step(hMod, encodedData);
+        receivedSignal = step(hChan, modSignal);
+        demodSignal    = step(hDemod, receivedSignal);
+        receivedBits   = step(hDec, demodSignal);
+        errorStats     = step(hError, data, receivedBits);
+        bitErr(i) = errorStats(1); 
+        parfor_progress;
     end
     
     I = [I; EbNo, mean(bitErr)];
-    
+    parfor_progress(0);
 end
