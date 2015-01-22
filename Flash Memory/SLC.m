e5bd4017485107be0e7cb1c5e02984383059e6a6
henryfletch <henry@villocq.com>
2015-01-22 17:52:50 +0000

    test

--- Flash Memory/SLC.m
+++ Flash Memory/SLC.m
@@ -4,23 +4,31 @@ clear
 close
 
 x = -5:0.0001:5;
-N = 1000;
+N = 10000;
 Verased = 1.4;
 Vp = 2.6;
 deltaVp = 0.25;
-time = 10e5;
+time = 10e7;
 
-yErased = normpdf(x,1.4,0.35);
+%Erased State
+yErased = normpdf(x,Verased,0.35);
+
+%Initial Programmed State Vp
 yProgrammed = mem_programState(x,Vp,deltaVp);
+
+%Noise functions
 yRTN = mem_RTN(x,N);
 yRetention = mem_retention(x,time,N,Verased,Vp);
 
+%Noised programme distribution
 output1 = conv(yProgrammed,yRTN,'same');
 output2 = conv(output1,yRetention,'same');
 
+%Noised erased distribution
 output3 = conv(yErased,yRTN,'same');
 output4 = conv(output3,yRetention,'same');
 
+%Normalise area = 1
 area2 = trapz(x,output2);
 output2 = output2/area2;
 
