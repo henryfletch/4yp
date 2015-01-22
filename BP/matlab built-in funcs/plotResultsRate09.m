df5e09b79bb2e86cc4ab8a20a345b0ca51e6ad41
henryfletch <henry@villocq.com>
2015-01-22 18:08:14 +0000

    graphs

---
+++ BP/matlab built-in funcs/plotResultsRate09.m
@@ -0,0 +1,31 @@
+clear
+
+%Input Data, Z
+
+my = load('./DVBS2 Results/myDecoder-rate0.9.mat');
+matlab = load('./DVBS2 Results/matlabDecoder-rate0.9.mat');
+
+% Uncoded function generator
+x_unenc = -6:0.01:7;
+x_unenc = 10.^(x_unenc./10);
+y_unenc = 0.5*erfc(sqrt(x_unenc));
+x_unenc = 10*log10(x_unenc); 
+
+%Extract x and y
+my_x = my.I(:,1) - 10*log10(1/0.9);
+my_y = my.I(:,3);
+matlab_x = matlab.I(:,1);
+matlab_y = matlab.I(:,2);
+
+%Semilog Plots
+my = semilogy(my_x,my_y,'DisplayName','N=64800,Rate=0.9,Personal Decoder','LineWidth',1.5);
+hold on
+grid on
+matlab = semilogy(matlab_x,matlab_y,'DisplayName','N=64800,Rate=0.9,Matlab decoder','LineWidth',1.5);
+uncoded = semilogy(x_unenc,y_unenc,'DisplayName','Uncoded','LineWidth',1.5);
+xlabel('Eb/No (dB)');
+ylabel('Bit Error Probability');
+title('AWGN Channel - Performance Curves');
+lgnd = legend([uncoded matlab my]); % coded1 coded3]);
+set(lgnd,'FontSize',12,'Location','SouthWest');
+axis([-6 6 10e-9 1])
