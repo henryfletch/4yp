dbaaed2d35638bec4666f13995b7a21014e1caf8
henryfletch <henry@villocq.com>
2015-02-04 12:39:19 +0000

    vectorised

--- Flash Memory/memoryGetVoltage.m
+++ Flash Memory/memoryGetVoltage.m
@@ -33,22 +33,13 @@ V0 = gen_uniform(SystemParams.Vp,SystemParams.Vp+SystemParams.deltaVp,samples);
 % Erased state (Vector)
 Ve = gen_gaussian(SystemParams.Verased,0.35,samples);
 
-% Now loop through b, detect if 1 or 0, then add correct noise
-y = zeros(1,length(b));
-for i = 1:length(b)
-    
-    if b(i) == 1
-        % Get Voltage for binary 1
-        y(i) = V0(i) + RTN(i) + retention(i);
-        
-    elseif b(i) == 0
-        % Get Voltage for binary 0
-        y(i) = Ve(i) + RTN(i);
-    else
-        error('Something very bad happened!');
-    end
-    
-end
+%%%
+% Matrix of vectors
+y_bin1 = V0 + RTN + retention;
+y_bin0 = Ve + RTN;
+
+y = b.*y_bin1 + ~b.*y_bin0;
+y=y';
 end
 
 
