a02109c46fb6a79f55b1c1c89256c57f23322266
henryfletch <henry@villocq.com>
2015-02-06 13:36:19 +0000

    update

--- Flash Memory/decoder/ZZ_run_sim.m
+++ Flash Memory/decoder/ZZ_run_sim.m
@@ -1,13 +1,25 @@
 function ZZ_run_sim(varargin)
 
-N = varargin{1};
+if isstr(varargin{1})
+    N = str2double(varargin{1});
+else
+    N = varargin{1};
+end
 
+[~,randomID] = strtok(tempname,'_');
+filename = strcat('output_files/',varargin{2},randomID,'.txt');
+
+% Ensures truly random numbers for each process
+% seed is now a random number that can be used to initialise rand
+fid2 = fopen('/dev/urandom');
+seed = fread(fid2, 1, 'uint32');
+RandStream.setDefaultStream(RandStream('mt19937ar','seed',seed));
 
 % LDPC Graphing Run Script
 % FIXED t, VARIABLE N
 
-addpath('../Random Generators');
-addpath('../.');
+%addpath('../Random Generators');
+%addpath('../.');
 
 % System Parameters
 SystemParams.tYrs = 5;
@@ -15,36 +27,31 @@ SystemParams.Verased = 1.4;
 SystemParams.Vp = 2.8;
 SystemParams.deltaVp = 0.25;
 
-% Retention Parameters
-retentionData.Ks = 0.38;
-retentionData.Kd = 4e-4;
-retentionData.Km = 4e-6;
-retentionData.t0 = 3600;
-
 %Code Rate
 Rc = 9/10;
 
 %DVB-S2 Parity check matrix
-H = dvbs2ldpc(Rc,'outputFormat','indices');
+H = dvbs2ldpc(Rc);
 
-% MC Simulation Runs
-mc_iters = 10;
+% Blocks per program
+mc_iters = 1000;
 
-% Loop to go over all values of EbNo, as well as perform MC Simulation
+hEnc = fec.ldpcenc(H);
+hDec = fec.ldpcdec(H);
+hDec.DoParityChecks = 'Yes';
+hDec.DecisionType = 'Hard decision';
+hDec.OutputFormat = 'Whole codeword';
+hDec.NumIterations = 50;
 
-    hEnc = comm.LDPCEncoder(H);
-    hDec = comm.LDPCDecoder('ParityCheckMatrix',H,'IterationTerminationCondition',...
-        'Parity check satisfied','OutputValue','Whole codeword');
-    hError = comm.ErrorRate;
+SystemParams.N = N;
+voltageHardDecision = decisionFunc(N);
 
-    SystemParams.N = N;
-    voltageHardDecision = decisionFunc(N);
-    %for Loop
-    for i = 1:mc_iters
-        errRatio(i) = ZZ_ldpc_BER_memoryN_coded(Rc,hEnc,hDec,hError,SystemParams,retentionData,voltageHardDecision);
-    end
+%for Loop
+for i = 1:mc_iters
+    errRatio(i) = ZZ_ldpc_BER_memoryN_coded(Rc,hEnc,hDec,SystemParams,voltageHardDecision);
+end
 
-fid=fopen('output','a+');
+fid=fopen(filename,'a+');
 fprintf(fid,'%e\n',mean(errRatio));
 
 end
