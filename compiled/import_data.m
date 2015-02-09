5023bd12102c184744d05a9313ea209487e988cf
henryfletch <henry@villocq.com>
2015-02-09 11:40:48 +0000

    update

--- compiled/import_data.m
+++ compiled/import_data.m
@@ -4,7 +4,7 @@ I = [];
 
 for i = EbNoRange
     display(i);
-    value = strcat('memhard', num2str(i),'_*');
+    value = strcat('memsoft', num2str(i),'_*');
     d = dir(value);
     names = {d.name};
     L = length(names);
