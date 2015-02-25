d1f63ed6d63fa4bc83ab554230c12416be7cd4b8
henryfletch <henry@villocq.com>
2015-02-25 00:13:50 +0000

    b

---
+++ BP/BP_iterate_old.m
@@ -0,0 +1,80 @@
+% Belief Propogation Iteration Function
+
+function [y,iterations] = BP_iterate(x,H,l)
+
+iterations = l;
+
+% x = Input LLR vector
+% H = Graph connection matrix/Parity Check Matrix
+% l = # Iterations
+% y = Output LLR vector
+
+%i = Message Nodes;
+%j = Check Nodes;
+
+% Need to calculate number of CHK and MSG nodes from H
+[j_max,i_max] = size(H);
+nonzeros = nnz(H);
+
+%Preallocate M_IJ and M_JI as sparse matrices
+m_IJ = spalloc(j_max,i_max,nonzeros);
+m_JI = spalloc(j_max,i_max,nonzeros);
+
+for iter = 0:l
+    %All Message nodes:
+    for i = 1:i_max
+        % At each Message node:
+        h = H(:,i);% Column vector of connections to check nodes
+        
+        % Message sent down each branch from MSG to CHK:j
+        if iter == 0 % on initial iteration:
+            m_IJ(find(h),i) = x(i); % Message sent = initial conditions
+        else % subsequently:
+            w = m_JI(:,i);
+            m_IJ(:,i) = h*x(i) + h*sum(w) - w;
+        end
+    end
+    
+    m_IJ_2 = m_IJ'; %Transpose: Memory access is then much quicker!
+    
+    %All Check nodes:
+    for j = 1:j_max
+        % At each Check node:
+        
+        % Message sent down each branch:
+        w = m_IJ_2(:,j);
+        [row,~,v] = find(w); % v is non-zero elements
+        m_JI(j,row) = 2*atanh(prod(tanh(v./2))./(tanh(v./2)));
+    end
+    
+    % NEW! Clipping function
+    m_JI((m_JI) > 1000)=999; 
+    m_JI((m_JI) < -1000)=-999;
+    
+    %Get current variable node values
+    sumVector = sum(m_JI);
+    y = x + sumVector;
+
+    %Check: Is a valid codeword? i.e. y*H' == 0
+    %Hard Decision:
+    for i = 1:length(y)
+        if y(i) > 0
+            y2(i) = 0;
+        else
+            y2(i) = 1;
+        end
+    end
+    %Parity Check:
+    test = sum(mod(y2*H',2));
+    if test == 0
+        iterations = iter;
+        return
+    end
+    
+end
+
+end
+
+
+
+
--- BP/ldpc_BER_AWGN.m
+++ BP/ldpc_BER_AWGN.m
@@ -2,7 +2,7 @@
 % and then decodes using Belief Propogation (iterations l),
 % finally displays BER.
 
-function [biterr_num,biterr_ratio,iterations] = ldpc_BER_AWGN(G,H,l,sigma2)
+function [biterr_num,biterr_ratio,iterations] = ldpc_BER_AWGN(G,H,l,sigma2,hDec)
 
 % Input vector
 [rows,cols] = size(G);
@@ -13,13 +13,8 @@ x = mod(x_message*G,2);
 x_encoded = x;
 
 %BPSK -> bin0 -> +1 and bin1 -> -1
-for i = 1:length(x)
-    if x(i) == 0
-        x(i) = 1;
-    else
-        x(i) = -1;
-    end
-end
+x(x==1) = -1;
+x(x==0) = 1;
 
 %AWGN Channel
 n = sqrt(sigma2)*randn(1,cols); % Noise vector
@@ -29,17 +24,19 @@ x = x + n;
 x = (2*x)/sigma2;
 
 % Belief Propogation Stage
-[y,iterations] = BP_iterate(x,H,l);
+%[y,iterations] = BP_iterate(x,H,l);
+[~,y] = hDec.decode(x,l);
+iterations = l;
 
 %Convert from LLR to Binary
-for i = 1:length(y)
-    if y(i) > 0
-        y(i) = 0;
-    else
-        y(i) = 1;
-    end
-end
+% for i = 1:length(y)
+%     if y(i) > 0
+%         y(i) = 0;
+%     else
+%         y(i) = 1;
+%     end
+% end
 
-[biterr_num,biterr_ratio] = biterr(x_encoded,y);
+[biterr_num,biterr_ratio] = biterr(x_encoded,+y);
 
 end
