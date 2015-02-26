80a5def311d470eb95cf66415c8860c5a447fc49
henryfletch <henry@villocq.com>
2015-02-26 11:47:22 +0000

    g

--- Flash Memory/decoder/opencl-LDPC/decoderScript.m
+++ Flash Memory/decoder/opencl-LDPC/decoderScript.m
@@ -23,7 +23,7 @@ H = dvbs2ldpc(Rc);
 %H2 = full(H);
 
 % MC Simulation Runs
-mc_iters = 500000;
+mc_iters = 10000;
 l = 50;
 
 % Loop to go over all values of EbNo, as well as perform MC Simulation
@@ -33,23 +33,28 @@ hEnc = comm.LDPCEncoder(H);
 hDec = ldpcdec(H, 'cl\Kernels_sp.cl', 0.875);
 hError = comm.ErrorRate;
 
-for N = 35000
-    fprintf('N =%6.2f',N);
-    fprintf('\n');
-    tic;
-    SystemParams.N = N;
-    %SystemParams.tYrs = timeFunc(N,alpha);
-    voltageHardDecision = decisionFunc(N);
-    %Parfor Loop
-    %parfor_progress(mc_iters);
-    for i = 1:mc_iters
-        errRatio(i) = ocl_ldpc_BER_memoryN_coded(Rc,hEnc,hDec,hError,SystemParams,voltageHardDecision,H,l);
-        %parfor_progress;
+for N = 37000:1000:45000
+    for t = 1:1:10
+        fprintf('N = %6.2f',N);
+        fprintf('\n');
+        fprintf('t = %6.2f',t);
+        fprintf('\n');
+        tic;
+        SystemParams.N = N;
+        SystemParams.tYrs = t;
+        %SystemParams.tYrs = timeFunc(N,alpha);
+        voltageHardDecision = decisionFunc(N);
+        %Parfor Loop
+        %parfor_progress(mc_iters);
+        for i = 1:mc_iters
+            errRatio(i) = ocl_ldpc_BER_memoryN_coded(Rc,hEnc,hDec,hError,SystemParams,voltageHardDecision,H,l);
+            %parfor_progress;
+        end
+        %parfor_progress(0);
+        toc;
+        %Output Matrix
+        I = [I;N,t,mean(errRatio)];
     end
-    %parfor_progress(0);
-    toc;
-    %Output Matrix
-    I = [I;N,mean(errRatio)];
 end
 hDec.delete();
 clear hdec;
