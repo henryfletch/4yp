b31312b68f489fc51590faf15de8dbd6ce96a991
henryfletch <henry@villocq.com>
2015-03-10 23:01:15 +0000

    b

--- Flash Memory/decoder/decoderScript.m
+++ Flash Memory/decoder/decoderScript.m
@@ -32,12 +32,12 @@ H = dvbs2ldpc(Rc);
 % Rc = 3367/4095;
 
 % MC Simulation Runs
-mc_iters = 500;
+mc_iters = 1000;
 l = 50;
 
 % Loop to go over all values of EbNo, as well as perform MC Simulation
 I = [];
-for N = 30000
+for N = 39000:1000:45000
     fprintf('N =%6.2f',N);
     fprintf('\n');
     hEnc = comm.LDPCEncoder(H); %G;
@@ -50,7 +50,7 @@ for N = 30000
     voltageHardDecision = 0;%decisionFunc(N);
     %Parfor Loop
     parfor_progress(mc_iters);
-    for i = 1:mc_iters
+    parfor i = 1:mc_iters
         errRatio(i) = ldpc_BER_memoryN_coded(Rc,Nc,hEnc,hDec,hError,SystemParams,voltageHardDecision,H,l);
         parfor_progress;
     end
--- Flash Memory/test_EbNo.m
+++ Flash Memory/test_EbNo.m
@@ -7,8 +7,9 @@ Verased = 1.4;
 
 for i = 1:length(N)
     [mu,sigma] = getRetentionParams(N(i),t,Vp,Verased);
-    ratio = ((2*Vp + deltaVp)^2)/(16*sigma^2);
-    ebno(i) = 10*log10(ratio);
+    ratioProg = (Vp^2)/((1/12)*deltaVp^2 + 2*(0.00025*N(i)^0.5)^2 + 2*sigma^2 + mu^2);
+    ratioErase  = (Verased^2)/(2*0.35^2 + 2*(0.00025*N(i)^0.5)^2);
+    ebno(i) = 10*log10((ratioProg+ratioErase)/2);
 end
 
 end
