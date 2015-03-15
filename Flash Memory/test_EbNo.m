732b96f9de3051bbc7b5aff42103c6035f596529
henryfletch <henry@villocq.com>
2015-03-15 11:16:34 -0400

    report first save

--- Flash Memory/test_EbNo.m
+++ Flash Memory/test_EbNo.m
@@ -7,7 +7,7 @@ Verased = 1.4;
 
 for i = 1:length(N)
     [mu,sigma] = getRetentionParams(N(i),t,Vp,Verased);
-    ratioProg = (Vp^2)/((1/12)*deltaVp^2 + 2*(0.00025*N(i)^0.5)^2 + 2*sigma^2 + mu^2);
+    ratioProg = (Vp+mu)^2/((1/12)*deltaVp^2 + 2*(0.00025*N(i)^0.5)^2 + 2*sigma^2);
     ratioErase  = (Verased^2)/(2*0.35^2 + 2*(0.00025*N(i)^0.5)^2);
     ebno(i) = 10*log10((ratioProg+ratioErase)/2);
 end
