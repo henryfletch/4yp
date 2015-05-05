fe3707c11e7432392409afeb7e742b44fa88adf3
henryfletch <henry@villocq.com>
2015-05-05 02:36:31 +0100

    f

--- Flash Memory/decoder/ldpc_BER_memoryN_coded_sim.m
+++ Flash Memory/decoder/ldpc_BER_memoryN_coded_sim.m
@@ -16,10 +16,27 @@ y = memoryGetVoltage(encodedData,SystemParams);
 
 % HARD DECISION process on Cell Voltage
 % > vHardDecision, then binary 1 (LLR -50), otherwise binary 0 (LLR +50)
-y(y <= voltageHardDecision) = 0;
-y(y > 0) = 1; 
-L(y == 0) = 5;
-L(y == 1) = -5;
+% y(y <= voltageHardDecision) = 0;
+% y(y > 0) = 1; 
+% L(y == 0) = 5;
+% L(y == 1) = -5;
+
+% Quantisation test
+% 8 Read Quantiser:
+% y(y>2.4) = 2.4;
+% y((2.3 < y)&(y < 2.4)) = 2.35;
+% y((2.2 < y)&(y < 2.3)) = 2.25;
+% y((2.1 < y)&(y < 2.2)) = 2.15;
+% y((2.0 < y)&(y < 2.1)) = 2.05;
+% y((1.9 < y)&(y < 2.0)) = 1.95;
+% y((1.8 < y)&(y < 1.9)) = 1.85;
+% y(y<1.8) = 1.8;
+
+%4 Read Quantiser:
+y(y>2.6) = 2.6;
+y((2.2 < y)&(y < 2.6)) = 2.4;
+y((1.8 < y)&(y < 2.2)) = 2.0;
+y(y<1.8) = 1.8;
 
 %%%% SOFT DECISION -> Generate a LLR using gaussian approximation
 %%%% L is the vector of log liklehood ratios
@@ -33,10 +50,10 @@ L(y == 1) = -5;
 %%% Actual function (Retention only)
 %L = llr_full(y,SystemParams.Verased,0.35,SystemParams);
 %%% Actual function - Hachem's - (Retention + RTN)
-%L = llr_full_hachem(y,SystemParams.Verased,0.35,SystemParams);
+L = llr_full_hachem(y,SystemParams.Verased,0.35,SystemParams);
 
 % Belief Propogation Stage
-receivedBits = decode(hDec, L);
+receivedBits = decode(hDec, L');
 receivedBits = +receivedBits;
 % Iterates on LLR, outputs binary 1,0
 
